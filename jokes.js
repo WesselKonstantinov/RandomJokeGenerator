19c2efd0a5dae7338acb2b0b6d3f981922661dd3
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-03-03 12:29:23 +0100

    Add file that stores all jokes

---
+++ jokes.js
@@ -0,0 +1,214 @@
+const animalJokes = [
+    {
+        setup: 'What time is it when an elephant steps on your golf ball?',
+        punchline: 'Time to get a new ball!'
+    },
+    {
+        setup: 'Why don\'t grasshoppers watch golf?',
+        punchline: 'They watch cricket instead.'
+    },
+    {
+        setup: 'Why did the deer need braces?',
+        punchline: 'He had buck teeth.'
+    },
+    {
+        setup: 'What kind of dog can jump as high as a tall building?',
+        punchline: 'Any kind. A building can\'t jump!'
+    },
+    {
+        setup: 'How do you stop a dog from barking in your front yard?',
+        punchline: 'Put him in your backyard!'
+    },
+    {
+        setup: 'What did the dog say when he sat on sandpaper?',
+        punchline: 'Rough! Rough!'
+    },
+    {
+        setup: 'What\'s more amazing than a talking dog?',
+        punchline: 'A spelling bee.'
+    },
+    {
+        setup: 'What do you call a lazy baby kangaroo?',
+        punchline: 'A pouch potato.'
+    },
+    {
+        setup: 'What do you call a pig that does karate?',
+        punchline: 'A pork chop.'
+    },
+    {
+        setup: 'What do you get if you cross a teddy bear with a pig?',
+        punchline: 'A teddy boar.'
+    }
+];
+
+const computerJokes = [
+    {
+        setup: 'Why did the computer go to the dentist?',
+        punchline: 'Because of its Bluetooth.'
+    },
+    {
+        setup: 'What do you call an iPhone that isn\'t kidding around?',
+        punchline: 'Dead Siri-ous'
+    },
+    {
+        setup: 'Why did the computer show up at work late?',
+        punchline: 'It had a hard drive.'
+    },
+    {
+        setup: 'How does a computer get drunk?',
+        punchline: 'It takes screenshots.'
+    },
+    {
+        setup: 'Why was the cell phone wearing glasses?',
+        punchline: 'It lost its contacts.'
+    },
+    {
+        setup: 'I just got fired from my job at the keyboard factory.',
+        punchline: 'They told me I wasn\'t putting in enough shifts.'
+    },
+    {
+        setup: 'Why did the PowerPoint Presentation cross the road?',
+        punchline: 'To get to the other slide.'
+    },
+    {
+        setup: 'Did you hear about the monkeys who shared an Amazon account?',
+        punchline: 'They were Prime mates.'
+    },
+    {
+        setup: 'What should you do after your Nintendo game ends in a tie?',
+        punchline: 'Ask for a Wii-match!'
+    },
+    {
+        setup: 'What is the biggest lie in the entire universe?',
+        punchline: '"I have read and agree to the Terms & Conditions."'
+    }
+];
+
+const birthdayJokes = [
+    {
+        setup: 'What do you get a hunter for his birthday?',
+        punchline: 'A birthday pheasant'
+    },
+    {
+        setup: 'What does a clam do on his birthday?',
+        punchline: 'He shellabrates!'
+    },
+    {
+        setup: 'Why was the birthday cake as hard as a rock?',
+        punchline: 'Because it was marble cake!'
+    },
+    {
+        setup: 'What do you say to your goldfish on his birthday?',
+        punchline: 'Have a fin-tastic day.'
+    },
+    {
+        setup: 'What did the teddy bear say when it was offered some birthday cake?',
+        punchline: 'No thanks, I\'m stuffed.'
+    },
+    {
+        setup: 'What\'s the easiest way to remember your wife\'s birthday?',
+        punchline: 'Forget it once!'
+    },
+    {
+        setup: 'What goes up and never comes down?',
+        punchline: 'Your age.'
+    },
+    {
+        setup: 'What sort of birthday cake do ghosts prefer?',
+        punchline: 'An "I scream" cake'
+    },
+    {
+        setup: 'Why did the boy feel warm on his birthday?',
+        punchline: 'Because people kept toasting him!'
+    },
+    {
+        setup: 'What did the pirate say on his 80th birthday?',
+        punchline: 'Aye, matey!'
+    }
+];
+
+const foodJokes = [
+    {
+        setup: 'Why doesn\'t McDonald\'s serve escargot?',
+        punchline: 'It\'s not fast food!'
+    },
+    {
+        setup: 'What is a tree\'s favorite soda?',
+        punchline: 'Root Beer.'
+    },
+    {
+        setup: 'Why did the butcher work extra hours at the shop?',
+        punchline: 'To make ends meat.'
+    },
+    {
+        setup: 'What do you call cheese that is sad?',
+        punchline: 'Blue cheese.'
+    },
+    {
+        setup: 'When do you go at red and stop at green?',
+        punchline: 'When you\'re eating a watermelon.'
+    },
+    {
+        setup: 'What do you call blueberries playing the guitar?',
+        punchline: 'A jam session.'
+    },
+    {
+        setup: 'Did you hear about the carrot detective?',
+        punchline: 'He got to the root of every case.'
+    },
+    {
+        setup: 'What do you get if you cross an apple with a shellfish?',
+        punchline: 'A crab apple!'
+    },
+    {
+        setup: 'Why does yogurt love going to museums',
+        punchline: 'Because it\'s cultured.'
+    },
+    {
+        setup: 'Why did the tomato blush?',
+        punchline: 'Because it saw the salad dressing.'
+    }
+];
+
+const sportsJokes = [
+    {
+        setup: 'Why should you never break up with a goalie?',
+        punchline: 'Because he is a keeper.'
+    },
+    {
+        setup: 'Why did the golfer wear two pair of trousers?',
+        punchline: 'In case he got a hole in one.'
+    },
+    {
+        setup: 'What do you call a boat full of polite football players?',
+        punchline: 'A good sportsman ship.'
+    },
+    {
+        setup: 'Why shouldn’t you marry a tennis player?',
+        punchline: 'Because Love means nothing to them.'
+    },
+    {
+        setup: 'The boating store was having a big sale on canoes.',
+        punchline: 'It was quite the oar deal.'
+    },
+    {
+        setup: 'Why couldn\'t the bicycle stand up by itself?',
+        punchline: 'It was two tired!'
+    },
+    {
+        setup: 'What do sprinters eat before a race?',
+        punchline: 'Nothing, they fast!'
+    },
+    {
+        setup: 'Swimming is a confusing sport',
+        punchline: 'Sometimes you do it for fun, and other times you do it to not die.'
+    },
+    {
+        setup: 'I don\'t play soccer because I enjoy the sport.',
+        punchline: 'I\'m just doing it for kicks.'
+    },
+    {
+        setup: 'Is the pool safe for swimming?',
+        punchline: 'It deep ends.'
+    }
+];
